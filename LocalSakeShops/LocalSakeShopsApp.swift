67d2b94a06f749fb2d6edc9959e51c92aa6c2498
Leonardo Medeiros <leohsmedeiros@gmail.com>
2026-06-23 11:57:54 -0300

    Create empty iOS project

---
+++ LocalSakeShops/LocalSakeShopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalSakeShopsApp.swift
+//  LocalSakeShops
+//
+//  Created by Leonardo Medeiros on 23/06/26.
+//
+
+import SwiftUI
+
+@main
+struct LocalSakeShopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
